fbd9a7ceab7d50486dc50c75143871dadcb3dc4b
devmanso <mansourquddus@protonmail.com>
2025-01-17 17:06:51 -0500

    Added a couple limelight methods, made a seperate file for constants besides tuner constants

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -13,7 +13,14 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 public class Limelight extends SubsystemBase {
   /** Creates a new Limelight. */
   public Limelight() {
+    setToAprilTagPipeline();
+  }
 
+  /**
+   * changes pipeline to 0 (apriltag pipeline)
+   */
+  public static void setToAprilTagPipeline() {
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(0);
   }
 
   @Override
@@ -37,7 +44,33 @@ public class Limelight extends SubsystemBase {
 
   }
 
-  // strictly for auton
+  /**
+   * Gets the distance from the limelight to the apriltag
+   * @param limelightMountAngleDegrees
+   * @param goalHeightInches
+   * @param limelightLensHeightInches
+   * @return distanceFromLimelightToGoalInches
+   * 
+   */
+  public static double getDistanceFromAprilTag(double limelightMountAngleDegrees, 
+  double goalHeightInches, double limelightLensHeightInches) {
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry ty = table.getEntry("ty");
+    double targetOffsetAngle_Vertical = ty.getDouble(0.0);
+
+    double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+    double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180);
+
+    // calculate distance
+    double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches)
+    / Math.tan(angleToGoalRadians);
+    return distanceFromLimelightToGoalInches;
+  }
+
+  /**
+   * Think getDistanceFromAprilTag, but for autonomous
+   * @return distanceFromLimelightToGoalInches
+   */
   public double autoEstimateDistance() {
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
     NetworkTableEntry ty = table.getEntry("ty");
@@ -56,10 +89,60 @@ public class Limelight extends SubsystemBase {
     double angleToGoalRadians = angelToGoalDegrees * (3.14159 / 180.0);
 
     // calculate distance
-    double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
+    double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches)
+     / Math.tan(angleToGoalRadians);
 
     // return distance
     return distanceFromLimelightToGoalInches;
+  }
+
+  /**
+   * 
+   * @return 1 if there is a valid target, 0 if not
+   */
+  public static double hasValidTargets() {
+    return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+  }
+
+  /**
+   * 
+   * @return vertical offset of crosshair to target
+   */
+  public static double getVerticalOffset() {
+    return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
+  }
+
+  /**
+   * 
+   * @return vertical offset of crosshair to target
+   */
+  public static double getHorizontalOffset() {
+    return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+  }
+
+  /**
+   * 
+   * @return AprilTag ID as double array
+   */
+  public static double[] getTagID() {
+    return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tid").getDoubleArray(new double[6]);
+  }
+
+  /**
+   * Distance estimation
+   * @param mountAngleDegrees
+   * @param lensHeightInches
+   * @param goalHeightInches
+   * @return distance
+   */
+  public static double estimateDistance(double mountAngleDegrees, double lensHeightInches, 
+  double goalHeightInches) {
+    double verticalOffset = getVerticalOffset();
+    double angleToGoalDegrees = mountAngleDegrees + verticalOffset;
+    double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180);
+    double distance = (goalHeightInches - lensHeightInches) / Math.tan(angleToGoalRadians);
+
+    return distance;
   }
 
 }
