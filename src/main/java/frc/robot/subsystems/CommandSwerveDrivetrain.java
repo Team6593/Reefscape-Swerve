1d00d72f8a8c31376211539d5e80f1ace717101e
ajahan2008 <studentnafi08@gmail.com>
2025-10-10 16:30:44 -0400

    rotation stuff

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -233,7 +233,7 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
                     // rotation .05 0 0
                     new PIDConstants(6.9, 0, 0),
                     // PID constants for rotation
-                    new PIDConstants(1, 0, 0)
+                    new PIDConstants(2.5, 0, 0)
                 ),
                 config,
                 // Assume the path needs to be flipped for Red vs Blue, this is normally the case
